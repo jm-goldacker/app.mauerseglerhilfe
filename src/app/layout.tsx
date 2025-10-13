4a87fd1f7eda50695b36ae68664ecc8ccb8c5b28
Johann Maximilian Goldacker <goldacker.johann@outlook.de>
2025-10-13 13:53:04 +0200

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import 'primereact/resources/themes/lara-light-indigo/theme.css';  // Theme
+import 'primereact/resources/primereact.min.css';                  // Core CSS
+import 'primeicons/primeicons.css';                                // Icons
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
